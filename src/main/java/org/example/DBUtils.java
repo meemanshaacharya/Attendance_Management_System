499ee44e90a7e22e7be7023414a68b680953b728
meemanshaacharya <meemanshaacharya2020@gmail.com>
2023-03-30 13:51:50 +0545

    Initial commit

---
+++ src/main/java/org/example/DBUtils.java
@@ -0,0 +1,145 @@
+package org.example;
+
+import org.example.models.Attendance;
+import org.example.models.Classes;
+import org.example.models.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBUtils {
+    public static final String TABLE_USER = "user";
+    public static final String TABLE_CLASS = "class";
+    public static final String TABLE_ATTENDANCE = "attendance";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_USERNAME = "username";
+    public static final String COLUMN_PASSWORD = "password";
+
+    public static final String COLUMN_CLASSNAME = "classname";
+    public static final String COLUMN_CLASSID = "classId";
+    public static final String COLUMN_USERID = "userId";
+
+
+    public static Connection connect() {
+        Connection connection = null;
+        String url = "jdbc:sqlite:src/main/resources/database/Attendance_Management_System.db";
+
+        try {
+            connection = DriverManager.getConnection(url);
+            System.out.println("Database Connection Successful");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
+    public static List<User> getUser(Connection connection, String name) {
+        String query = "SELECT * FROM " + TABLE_USER + " WHERE " + COLUMN_USERNAME + " LIKE '%" + name + "%'";
+        List<User> users = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()){
+                int id = resultSet.getInt(COLUMN_ID);
+                String username = resultSet.getString(COLUMN_USERNAME);
+                String password = resultSet.getString(COLUMN_PASSWORD);
+
+                User user = new User(username,password);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return users;
+    }
+
+    public static void addUsers (User modelUser, Connection connection){
+        String sql = "INSERT INTO " + TABLE_USER + "(" + COLUMN_USERNAME + "," + COLUMN_PASSWORD + ") " + "VALUES(?,?)";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, modelUser.getUsername());
+            pstmt.setString(2, modelUser.getPassword());
+            pstmt.executeUpdate();
+
+            pstmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addAttendance(Attendance modelAttendance, Connection connection){
+        String sql = "INSERT INTO " + TABLE_ATTENDANCE + "(" + COLUMN_CLASSID + "," + COLUMN_USERID +") " + "VALUES(?,?)";
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1,modelAttendance.getClassId());
+            pstmt.setInt(2,modelAttendance.getUserId());
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Attendance> getAttendance(Connection connection, Integer id) {
+        String query = "SELECT * FROM " + TABLE_ATTENDANCE + " WHERE " + COLUMN_ID + " = " + id;
+        List<Attendance> attendances = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()){
+                int ids = resultSet.getInt(COLUMN_ID);
+                int classID = resultSet.getInt(COLUMN_CLASSID);
+                int userId = resultSet.getInt(COLUMN_USERID);
+
+                Attendance attendance = new Attendance(classID,userId);
+                attendances.add(attendance);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return attendances;
+    }
+
+
+    public static void addClass(Classes modelClass, Connection connection) {
+
+        String sql = "INSERT INTO " + TABLE_CLASS + "(" + COLUMN_CLASSNAME + ") " +
+                "VALUES(?)";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, modelClass.getClassname());
+            pstmt.executeUpdate();
+            pstmt.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Classes> getClasses(Connection connection, String name) {
+        String query = "SELECT * FROM " + TABLE_CLASS + " WHERE " + COLUMN_CLASSNAME + " = " + name;
+        List<Classes> classesArrayList = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt(COLUMN_ID);
+                String classname = resultSet.getString(COLUMN_CLASSNAME);
+
+                Classes classes = new Classes(classname);
+                classesArrayList.add(classes);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return classesArrayList;
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import org.example.models.Attendance;
+import org.example.models.Classes;
+import org.example.models.User;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Connection connection = DBUtils.connect();
+        String username;
+        String password;
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter username: ");
+        username = scanner.nextLine();
+        System.out.println("Enter password:");
+        password = scanner.nextLine();
+
+        User user1 = new User(username, password);
+        DBUtils.addUsers(user1, connection);
+
+        List<User> userList = DBUtils.getUser(connection, "Sitaa");
+        for (User user : userList) {
+            System.out.println(user.getUsername() + " " + user.getPassword());
+        }
+        String classname;
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter classname:");
+        classname = scanner2.nextLine();
+
+        Classes classes00 = new Classes(classname);
+        DBUtils.addClass(classes00, connection);
+
+            List<Classes> classesList = DBUtils.getClasses(connection, "Lobuche");
+            for (Classes classes : classesList) {
+                System.out.println(classes.getClassname());
+            }
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter classId: ");
+        int classId = scanner1.nextInt();
+        System.out.println("Enter userId:");
+        int userId = scanner1.nextInt();
+        scanner1.close();
+
+
+        try {
+            Attendance attendance = new Attendance(classId, userId);
+            DBUtils.addAttendance(attendance, connection);
+            System.out.println("Attendence successfull");
+        } catch (Exception e) {
+            System.out.println("Failed to add attendance: " + e.getMessage());
+        }
+    }
+}
